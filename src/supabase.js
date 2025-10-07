e61b45f940253744fa84033083a9835e7fa57988
cristianconpe <cristian30082000@gmail.com>
2025-10-06 18:08:18 -0600

    Update supabase.js

--- src/supabase.js
+++ src/supabase.js
@@ -1,6 +1,7 @@
+// src/supabase.js
 import { createClient } from "@supabase/supabase-js";
 
-const SUPABASE_URL = "https://hgdkahwximatlgtwqgph.supabase.co";
-const SUPABASE_ANON_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImhnZGthaHd4aW1hdGxndHdxZ3BoIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTk3NzU2NTIsImV4cCI6MjA3NTM1MTY1Mn0.aRvTlFuADw49xuRRQ9E_zAxXVvyxt7elePd0bFwSF5o";
+const SUPABASE_URL = import.meta.env.VITE_SUPABASE_URL;
+const SUPABASE_ANON_KEY = import.meta.env.VITE_SUPABASE_ANON_KEY;
 
 export const supabase = createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
